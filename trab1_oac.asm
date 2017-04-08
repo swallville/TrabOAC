055f05fe328037faca493b9f745099a25bffc36c
Eduardo Calil <eduardosaidcalil@gmail.com>
2017-04-08 17:13:47 -0300

    Codigo fonte inicial

---
+++ trab1_oac.asm
@@ -0,0 +1,255 @@
+#-------------------------------------------------------------------------
+#		Organização e Arquitetura de Computadores - Turma C 
+#			Trabalho 1 - Programação Assembler
+#
+# Nome:  Eduardo Said Calil Vilaça		Matrícula: 13/0154253 
+# Nome:  Lukas 			        	Matrícula: 12/01
+# Nome:  Raphael 				Matrícula: 13/0154
+
+.data
+
+image_name:   	    .asciiz "lenaeye.raw"   # nome da imagem a ser carregada
+address: 	    .word   0x10040000	        # endereco do bitmap display na memoria	
+buffer:		    .word   0		                # configuracao default do MARS
+size:		    .word   4096		# numero de pixels da imagem
+
+
+.text
+
+j inicializa
+
+.macro print_int (%x)
+	li $v0, 1
+	add $a0, $zero, %x
+	syscall
+.end_macro
+
+.macro get_int
+	li $v0, 5             # Pega a opção inserida
+	syscall
+.end_macro
+
+.macro print_str (%str)
+	.data
+		myLabel: .asciiz %str
+	.text
+		li $v0, 4
+		la $a0, myLabel
+		syscall
+.end_macro
+
+# inicializa as variáveis globais
+inicializa:
+	lw $a1, address
+  	la $a2, buffer
+  	lw $a3, size
+
+# Mostra o menu de opções principal
+mostra_menu:
+	print_str("Defina o número da opção desejada:")
+	print_str("\n")
+
+	print_str("1- Obtém ponto")
+	print_str("\n")
+	
+	print_str("2- Desenha ponto")
+	print_str("\n")
+	
+	print_str("3- Desenha retângulo sem preenchimento")
+	print_str("\n")
+	
+	print_str("4- Converte para negativo da imagem")
+	print_str("\n")
+	
+	print_str("5- Carrega imagem")
+	print_str("\n")
+	
+	print_str("6- Encerra:")
+	print_str("\n")
+	
+	get_int
+
+# faz um switch-case com a opção inserida no menu principal
+define_opcao:
+	beq $v0, 1, get_point
+	beq $v0, 2, draw_point
+	beq $v0, 3, draw_empty_retangle
+	beq $v0, 4, convert_negative
+	beq $v0, 5, load_image
+	beq $v0, 6, exit
+	
+# Pergunta o ponto e mostra ele no display
+get_point:
+	print_str("Insira a coordenada x do ponto desejado: ")
+	get_int
+	move $t0, $v0
+	
+	print_str("Insira a coordenada y do ponto desejado: ")
+	get_int
+	move $t1, $v0
+	
+	## Pega componentes de X #
+	
+	# Pega componente B de x
+	andi $t2, $t0, 0x000000FF
+	
+	# Pega componente G de x
+
+	srl $t3, $t0, 8
+	andi $t3, $t3, 0x000000FF
+	
+	# Pega componente R de x
+	
+	srl $t4, $t0, 16
+	andi $t4, $t4, 0x000000FF
+	
+	print_str("\n")
+	print_str("Os componentes RGB da coordenada x são:")
+	print_str("\n")
+	
+	print_str("R: ")
+	print_int($t4)
+	print_str("\t")
+
+	print_str("G: ")
+	print_int($t3)
+	print_str("\t")
+
+	print_str("B: ")
+	print_int($t2)
+	print_str("\n")
+	
+	## Pega componentes de Y #
+	
+	# Pega componente B de y
+	andi $t2, $t1, 0x000000FF
+	
+	# Pega componente G de y
+
+	srl $t3, $t1, 8
+	andi $t3, $t3, 0x000000FF
+	
+	# Pega componente R de y
+	
+	srl $t4, $t1, 16
+	andi $t4, $t4, 0x000000FF
+	
+	print_str("\n")
+	print_str("Os componentes RGB da coordenada y são:")
+	print_str("\n")
+	
+	print_str("R: ")
+	print_int($t4)
+	print_str("\t")
+
+	print_str("G: ")
+	print_int($t3)
+	print_str("\t")
+
+	print_str("B: ")
+	print_int($t2)
+	print_str("\t")
+	
+	j exit
+	
+draw_point:
+	print_str("Insira a coordenada x do ponto desejado: ")
+	get_int
+	move $t0, $v0
+	
+	print_str("Insira a coordenada y do ponto desejado: ")
+	get_int
+	move $t1, $v0
+	
+	print_str("Insira a cor do ponto: ")
+	get_int
+	move $t2, $v0
+
+	j exit
+	
+draw_empty_retangle:
+	print_str("Insira a coordenada x do primeiro ponto desejado: ")
+	get_int
+	move $t0, $v0
+	
+	print_str("Insira a coordenada y do primeiro ponto desejado: ")
+	get_int
+	move $t1, $v0
+	
+	print_str("Insira a coordenada x do segundo ponto desejado: ")
+	get_int
+	move $t2, $v0
+	
+	print_str("Insira a coordenada y do segundo ponto desejado: ")
+	get_int
+	move $t3, $v0
+	
+	print_str("Insira a cor do ponto: ")
+	get_int
+	move $t4, $v0
+
+	j exit
+	
+convert_negative:
+	j exit
+
+  #-------------------------------------------------------------------------
+  # Funcao load_image: carrega uma imagem em formato RAW RGB para memoria
+  # Formato RAW: sequencia de pixels no formato RGB, 8 bits por componente
+  # de cor, R o byte mais significativo
+  #
+  # Parametros:
+  #  $a0: endereco do string ".asciiz" com o nome do arquivo com a imagem
+  #  $a1: endereco de memoria para onde a imagem sera carregada
+  #  $a2: endereco de uma palavra na memoria para utilizar como buffer
+  #  $a3: tamanho da imagem em pixels
+  #
+  # A função foi implementada ...
+  
+load_image:
+  # carrega imagem --------------------
+
+  la $a0, image_name
+
+  # salvar parametros da funcao nos termporarios
+  move $t7, $a0	     # nome do arquivo
+  move $t8, $a1      # endereco de carga
+  move $t9, $a2	     # buffer para leitura de um pixel do arquivo
+  
+  li   $v0, 13       # system call para abertura de arquivo
+  li   $a1, 0        # Abre arquivo para leitura (parâmtros são 0: leitura, 1: escrita)
+  li   $a2, 0        # modo é ignorado
+  syscall            # abre um arquivo (descritor do arquivo é retornado em $v0)
+  move $t6, $v0      # salva o descritor do arquivo
+  
+  move $a0, $t6      # descritor do arquivo 
+  move $a1, $t9      # endereço do buffer 
+  li   $a2, 3        # largura do buffer
+  
+loop:  
+
+  beq  $a3, $zero, close
+  
+  li   $v0, 14       # system call para leitura de arquivo
+  syscall            # lê o arquivo
+  lw   $t0, 0($a1)   # lê pixel do buffer	
+  sw   $t0, 0($t8)   # escreve pixel no display
+  addi $t8, $t8, 4   # próximo pixel
+  addi $a3, $a3, -1  # decrementa countador
+  
+  j loop
+  
+  # fecha o arquivo 
+close:  
+  li   $v0, 16       # system call para fechamento do arquivo
+  move $a0, $t6      # descritor do arquivo a ser fechado
+  syscall            # fecha arquivo
+  	
+  j exit
+  
+  
+  exit:
+  	  li $v0 10		# encerra programa
+  	  syscall
+  
+  
